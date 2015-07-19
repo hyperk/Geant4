e9ce14f50e7286a319c46489db22bf822d27c341
Francesca Di Lodovico <f.di.lodovico@qmul.ac.uk>
2015-07-19 11:51:07 +0100

    restructuring package for new geant4 release

--- source/examples/advanced/xray_fluorescence/readPY.py
+++
@@ -1,6 +0,0 @@
-from AidaProxy import *
-treeSim = Proxy_Store("xrayfluo.xml","xml",3)
-histoSim = treeSim.retrieveH1D("1")
-from rootPlotter2 import *  
-pl = RootPlotter()          
-pl.plot(histoSim)
