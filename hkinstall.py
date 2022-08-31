ab500d78c27d7c84d2b7fa760f042ac8bbce891c
Mathieu Guigue <mgguigue@gmail.com>
2022-08-31 20:03:29 +0200

    Fix post installation

--- hkinstall.py
+++ hkinstall.py
@@ -1,8 +1,10 @@
 from hkpilot.utils.cmake import CMake
+from hkpilot.utils import fancylogger
 
 import inspect
 import os
 
+logger = fancylogger.getLogger(__name__)
 
 class Geant4(CMake):
 
@@ -10,13 +12,25 @@ class Geant4(CMake):
         super().__init__(path)
 
         self._package_name = "Geant4"
-        self._package_version = "4.11.0.2"
-        self._download_url = "https://geant4-data.web.cern.ch/releases/geant4-v11.0.2.zip"
+        self._package_version = "10.1.3"
+        self._download_url = "http://cern.ch/geant4-data/releases/geant4_10_01_p03.zip"
 
-        self._cmakelist_path = "src/geant4-v11.0.2"
+        self._cmakelist_path = "src/geant4_10_01_p03"
 
         self._cmake_options = {
-            "WITH_TLS": "OFF"
+            "WITH_TLS": "OFF",
+            "GEANT4_INSTALL_DATA": "ON"
         }
 
         # self._path = os.path.relpath(inspect.getfile(self.__class__))
+    
+    def post_install(self):
+        logger.info(f"Post-installation of {self._package_name} in progress...")
+        src = f"{os.environ.get('HK_WORK_DIR')}/{self._package_name}/install-{os.environ.get('HK_SYSTEM')}/lib64/{self._package_name}-{self._package_version}"
+        dst = f"{os.environ.get('HK_WORK_DIR')}/{self._package_name}/install-{os.environ.get('HK_SYSTEM')}/lib64/cmake"
+        logger.debug(f"Creating symlink between {src} and {dst}")
+        if os.path.exists(dst):
+            os.unlink(dst)
+        os.symlink(src, dst, target_is_directory=True)
+        logger.info(f"Post-installation of {self._package_name} done successfully")
+        return True
