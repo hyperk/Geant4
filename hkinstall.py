502bae4b268da45ffc22abf82f9075f3c2dac57e
Mathieu Guigue <mgguigue@gmail.com>
2022-10-18 19:20:00 +0200

    Attempt CI

--- hkinstall.py
+++ hkinstall.py
@@ -6,12 +6,12 @@ import os
 
 logger = fancylogger.getLogger(__name__)
 
-class Geant4(CMake):
+class HKGeant4(CMake):
 
     def __init__(self, path):
         super().__init__(path)
 
-        self._package_name = "Geant4"
+        self._package_name = "hk-Geant4"
         self._package_version = "10.1.3"
         self._download_url = "http://cern.ch/geant4-data/releases/geant4_10_01_p03.zip"
 
@@ -22,8 +22,6 @@ class Geant4(CMake):
             "GEANT4_INSTALL_DATA": "ON"
         }
 
-        # self._path = os.path.relpath(inspect.getfile(self.__class__))
-    
     def post_install(self):
         logger.info(f"Post-installation of {self._package_name} in progress...")
         src = f"{os.environ.get('HK_WORK_DIR')}/{self._package_name}/install-{os.environ.get('HK_SYSTEM')}/lib64/{self._package_name}-{self._package_version}"
